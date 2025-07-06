c7b39b897f43f073808fc64c9e8a06e616d4eecd
starfall101 <184286666+starfall218@users.noreply.github.com>
2025-07-06 22:29:42 +0100

    Update othersectionsHeader.jsx

--- src/components/dashboard/components/othersectionsHeader.jsx
+++ src/components/dashboard/components/othersectionsHeader.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Header.css'; 
+import './header.css'; 
 
 const Header = ({toggleSidebar, name}) => {
     const userDataStore = localStorage.getItem('signupData');
